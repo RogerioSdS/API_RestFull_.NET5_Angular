71fd9357b629c7879aa053c8595f909976808c5a
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-03-22 09:31:42 -0300

    Finalizando a integração do EF Core no APP

--- MyAPI.NET5_Angular/Angular_NET5_APP/MyFirstWebAPPWithAngular/Controllers/EventoController.cs
+++ MyAPI.NET5_Angular/Angular_NET5_APP/MyFirstWebAPPWithAngular/Controllers/EventoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyFirstWebAPPWithAngular.Models;
+using MyFirstWebAPPWithAngular.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,43 +12,23 @@ namespace MyFirstWebAPPWithAngular.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new List<Evento>()
-        {
-            new Evento()
-            {
-            EventoId = 1,
-            Tema = "Angular 11 e .Net 5",
-            Local = "Belo Horizonte",
-            Lote = "1º Lote",
-            QtdPessoas = 250,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            ImagemURL = "imagem.png"
-           },
-           new Evento()
-            {
-            EventoId = 2,
-            Tema = "Angular 11 e .Net 5 e suas novidades",
-            Local = "São Paulo",
-            Lote = "2º Lote",
-            QtdPessoas = 350,
-            DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
-            ImagemURL = "imagem2.png"
-           }
-        };
-        public EventoController()
-        {
+       
+        private readonly DataContext _context;
 
+        public EventoController(DataContext context)
+        {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()//posso colocar qualquer nome no metodo, o que manda nesse caso é o atributo do controller [HttpGet]
         {
-            return _evento;
+            return _context.Eventos;
         }
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)//posso colocar qualquer nome no metodo, o que manda nesse caso é o atributo do controller [HttpGet]
+        public Evento GetById(int id)//posso colocar qualquer nome no metodo, o que manda nesse caso é o atributo do controller [HttpGet]
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
