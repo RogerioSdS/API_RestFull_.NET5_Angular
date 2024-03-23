0be0e191efa1042e756e125750802b4901ca7267
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-03-23 16:49:13 -0300

    criando metodo de request do angular na nossa API

--- MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/app.module.ts
+++ MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// HttpClientModule é um módulo do Angular que permite fazer requisições HTTP no backend
+// através do serviço HttpClient. Ele é necessário para realizar requisições HTTP no projeto.
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
@@ -14,9 +18,13 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HttpClientModule é um módulo do Angular que permite fazer requisições HTTP de forma fácil e intuitiva.
+    // É usado para buscar dados de APIs ou servidores.
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,23 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any =[
-    {
-    Tema: 'Angular',
-    Local: 'Birigui'
-  },
-  {
-    Tema: '.Net5',
-    Local: 'Araçatuba'
-  },
-  {
-    Tema: 'Angular 12 e suas novidades',
-    Local: 'São José do rio preto'
-  }
-  ]
-  constructor() { }
+  public eventos: any ;
+  constructor(private http: HttpClient) { }
 
+  /* O ngOnInit é um método do ciclo de vida do componente no Angular que é chamado automaticamente 
+  após a inicialização do componente. É usado para realizar inicializações ou configurações 
+  necessárias, como buscar dados de serviços, configurar variáveis ou assinar eventos. 
+  É recomendado para tarefas que precisam ser executadas apenas uma vez durante o ciclo de vida 
+  do componente.*/
   ngOnInit(): void {
+    this.getEventos();
   }
 
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    )
+  }
 }
