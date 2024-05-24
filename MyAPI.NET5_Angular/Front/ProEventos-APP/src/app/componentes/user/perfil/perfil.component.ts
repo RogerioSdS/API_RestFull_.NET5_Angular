a7a6287bd10350a50a931f1c3cbc658984ddb08e
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-05-24 16:26:25 -0300

    Criando compos obrigátorios, Criando validação para senhas (Must Match)

--- MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/componentes/user/perfil/perfil.component.ts
+++ MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/componentes/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  get f(): any { return this.form.controls; }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'confirmePassword')
+    };
+
+    this.form = this.fb.group({
+      titulo: [''],
+      primeiroNome: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      ultimoNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      password: ['', [Validators.minLength(6) , Validators.nullValidator]], //Verifica se o campo é nulo, se não for, ele passa o minimo de 6 caracteres
+      confirmePassword: ['', Validators.nullValidator],
+    }, formOptions);
+  };
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+  public resetForm(event : any): void {
+    event.preventDefault();
+    this.form.reset();
+    this.form.get('titulo')?.setValue('');
   }
 
 }
+
+
--- MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/componentes/user/registration/registration.component.ts
+++ MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/componentes/user/registration/registration.component.ts
@@ -1,4 +1,8 @@
+import { ValidatorField } from './../../../helpers/ValidatorField';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private toaster: ToastrService) { }
+
+  get f(): any { return this.form.controls; }
 
   ngOnInit(): void {
+    this.validation();
   }
 
+  private validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('password', 'confirmePassword')
+    };
+
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['',
+        [Validators.required, Validators.email]
+      ],
+      userName: ['', Validators.required],
+      password: ['',
+        [Validators.required, Validators.minLength(4)]
+      ],
+      confirmePassword: ['', Validators.required],
+    }, formOptions);
+  }
 }
