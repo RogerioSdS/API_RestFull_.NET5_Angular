f4cf709ee29f907df48bc844ad097edaeef74fb6
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-06-24 16:00:02 -0300

    instalando .Net Identity, Novas classes Domain, Heranças das classes

---
+++ MyAPI.NET5_Angular/Angular_NET5_APP/ProEventos.Domain/Identity/Role.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProEventos.Domain.Identity
+{
+    public class Role : IdentityRole<int>
+    {
+        public List<UserRole> UserRoles { get; set; }
+        
+    }
+}
