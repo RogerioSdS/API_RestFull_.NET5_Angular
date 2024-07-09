eeb317802281f9459638e78c23f6aa7059e2c8de
Rogerio Soares <rogerio.soares@frenet.com.br>
2024-07-09 18:29:24 -0300

    Paginação Realizada

--- MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/services/evento.service.ts
+++ MyAPI.NET5_Angular/Front/ProEventos-APP/src/app/services/evento.service.ts
@@ -1,61 +1,76 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Evento } from './../models/Evento';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Evento } from '../models/Evento';
-import { take } from 'rxjs/operators';
+import { take, map } from 'rxjs/operators';
 import { environment } from '@environments/environment';
+import { PaginatedResult } from '@app/models/Pagination';
 
-@Injectable()
-//{  providedIn: 'root', //Indica que esse provider deve ser resolvido na raiz do projeto, podendo ser utilizado
-// em outros componentes} uma outra maneira de criar um injeção de dependência
+@Injectable(
+// { providedIn: 'root'}
+)
 export class EventoService {
   baseURL = environment.apiURL + 'api/eventos';
 
   constructor(private http: HttpClient) { }
 
-  public getEventos() : Observable<Evento[]> {
-    return this.http.get<Evento[]>(this.baseURL)
-    .pipe(take(1));
-  }
+  public getEventos(page?: number, itemsPerPage?: number, term?: string): Observable<PaginatedResult<Evento[]>> {
+    const paginatedResult: PaginatedResult<Evento[]> = new PaginatedResult<Evento[]>();
 
-  /*um Observable é usado para lidar com operações assíncronas e fluxos de dados ao longo do tempo.
-  Ele é parte da programação reativa e permite tratar múltiplos valores assíncronos.
-  Observables são implementados usando a biblioteca RxJS e são amplamente utilizados para lidar com eventos
-  de DOM, chamadas de API HTTP e outras operações assíncronas em um aplicativo Angular.*/
+    let params = new HttpParams;
 
-  public getEventosByTema(tema: string): Observable<Evento[]> {
-    return this.http.get<Evento[]>(`${this.baseURL}/tema/${tema}`)
-    .pipe(take(1));
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page.toString());
+      params = params.append('pageSize', itemsPerPage.toString());
+    }
+
+    if (term != null && term != '')
+      params = params.append('term', term)
+
+    return this.http
+      .get<Evento[]>(this.baseURL, {observe: 'response', params })
+      .pipe(
+        take(1),
+        map((response) => {
+          paginatedResult.result = response.body;
+          if(response.headers.has('Pagination')) {
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+          }
+          return paginatedResult;
+        }));
   }
 
   public getEventoById(id: number): Observable<Evento> {
-    return this.http.get<Evento>(`${this.baseURL}/${id}`)
-    .pipe(take(1));
+    return this.http
+      .get<Evento>(`${this.baseURL}/${id}`)
+      .pipe(take(1));
   }
 
-  public post(evento : Evento): Observable<Evento> {
-    return this.http.post<Evento>(this.baseURL , evento)
-    .pipe(take(1));
+  public post(evento: Evento): Observable<Evento> {
+    return this.http
+      .post<Evento>(this.baseURL, evento)
+      .pipe(take(1));
   }
 
-  public put(evento : Evento): Observable<Evento> {
-    return this.http.put<Evento>(`${this.baseURL}/${evento.eventoId}`, evento)
-    .pipe(take(1));
+  public put(evento: Evento): Observable<Evento> {
+    return this.http
+      .put<Evento>(`${this.baseURL}/${evento.eventoId}`, evento)
+      .pipe(take(1));
   }
 
-  public deleteEvento(id: number): Observable<any>{
-    return this.http.delete(`${this.baseURL}/${id}`)
-    .pipe(take(1));
+  public deleteEvento(id: number): Observable<any> {
+    return this.http
+      .delete(`${this.baseURL}/${id}`)
+      .pipe(take(1));
   }
 
-  public postUpload(eventoId: number, file: File): Observable<Evento> {
+  postUpload(eventoId: number, file: File): Observable<Evento> {
     const fileToUpload = file[0] as File;
     const formData = new FormData();
     formData.append('file', fileToUpload);
 
     return this.http
-    .post<Evento>(`${this.baseURL}/upload-image/${eventoId}`, formData)
-    .pipe(take(1));
+      .post<Evento>(`${this.baseURL}/upload-image/${eventoId}`, formData)
+      .pipe(take(1));
   }
-
 }
